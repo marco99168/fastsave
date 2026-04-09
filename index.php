bc262d977b804c8b966a3b21f3bfb9a53340a207
marco99168 <787416091@qq.com>
2026-04-09 17:33:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 // ====================== 调试模式（成功后可删除） ======================
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 // ====================== CORS ======================
 header('Access-Control-Allow-Origin: *');
