84b82f48e03009213ab7303bd076a2c216361318
marco99168 <787416091@qq.com>
2026-04-09 15:43:53 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-$host = 'sql113.infinityfree.com';
-$dbname = 'if0_39872841_ar'; // 请确认实际数据库名
-$username = 'if0_39872841';
-$password = 'if0_39872841';
+$host = 'mainline.proxy.rlwy.net:32183';
+$dbname = 'railway'; // 请确认实际数据库名
+$username = 'root';
+$password = 'hNCzAqTCVbXjhYDGWyFHBOocSBkQnxIe';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
